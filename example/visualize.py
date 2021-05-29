c43f7713505b41139d2c5ab910ded490e15fc599
rifki2 <rifki@nodeflux.io>
2021-05-29 10:43:08 +0700

    add visualization script

--- example/visualize.py
+++ example/visualize.py
@@ -1,12 +1,29 @@
-from wordvector.utils import plot_with_matplotlib, plot_with_plotly
+from wordvector.utils import plot_with_matplotlib
+from wordvector.utils import cluster_dbscan, reduce_dimensions
+from gensim.models import Word2Vec 
+from glove import Glove 
+import joblib
 
-x_vals, y_vals, labels = reduce_dimensions(model)
+model = "word2vec"
+model = "glove"
+cluster_path = ""
+model_path = "/media/user/DATA/my_repo/word-vector/logs/glove_wikipedia/wikipedia_glove.model"
 
-try:
-    get_ipython()
-except Exception:
-    plot_function = plot_with_matplotlib
-else:
-    plot_function = plot_with_plotly
 
-plot_function(x_vals, y_vals, labels)
+if model == "glove":
+    model = Glove().load(model_path)
+    vectors = model.word_vectors
+    idx2key = model.inverse_dictionary
+    
+elif model == "word2vec":
+    model = Word2Vec.load(model_path)
+    vectors = model.wv.vectors
+    idx2key = model.wv.index_to_key
+
+# clustering
+labels = joblib.load(cluster_path)
+
+# reduce dimensions
+x_vals, y_vals = reduce_dimensions(vectors)
+
+plot_with_matplotlib(x_vals, y_vals, idx2key, cluster=labels)
